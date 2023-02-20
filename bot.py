326da5cd1fd73ccaa7835c0ff1ff3d2aa5afbd20
julellisg <julianellisg@gmail.com>
2023-02-20 01:28:55 -0500

    Update bot.py
    Added profile picture fetcher

--- bot.py
+++ bot.py
@@ -2,22 +2,28 @@ import os
 import discord
 from discord.ext import commands
 
+
 client = commands.Bot(command_prefix="=", intents=discord.Intents.all())    # set prefix, and allow bot to accept all events
 
 @client.event   # discord.py wrapper function
 async def on_ready():
+    await client.tree.sync()
     print('Connected to Discord')
+
+# Ping command but using slash
+@client.tree.command(name="ping", description="Sends the bot's latency in milliseconds (ms).")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f"ping! *{round(client.latency*1000)}ms*")
 
 # Ping command 
 @client.command()
 async def ping(ctx):                    # when someone says "ping"
     latency = round(client.latency * 1000)
-    await ctx.message.reply(f'pong! (*{latency}ms)*')
+    await ctx.message.reply(f'pong! *{latency}ms*')
 
 # Poll command
 @client.command()
 async def poll(ctx, *message):
-
     # check if empty first
     if len(message) <= 2:
         embed = discord.Embed(
@@ -42,14 +48,28 @@ async def poll(ctx, *message):
         color=0xFF9900
     )
 
+    # add each of the options into embedded message
     for i, option in enumerate(substring[1:]):
         emoji = emojis[i]
         embed.add_field(name=f"{emoji}\t{option}", value='', inline=False)
 
+    # print the embedded message and react onto the message using emojis
     message = await ctx.send(embed=embed)
     for i, option in enumerate(substring[1:]):
         emoji = emojis[i]
         await message.add_reaction(emoji)
 
+# Retrieve user profile picture
+@client.command()
+async def profile(ctx, member: discord.Member):
+    member_name = member.name + member.discriminator
+    pfp_url = member.avatar.url # retrieves avatar url of @mention user
+    embed = discord.Embed(
+        title="Avatar",
+        description=member_name,
+        color=0xFF9900
+    )
+    embed.set_image(url=pfp_url)
+    await ctx.message.reply(embed=embed)
 
 client.run(os.getenv('DISCORD_TOKEN'))
